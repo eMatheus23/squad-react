012519e8dafda72d2d9a858d17cd031be0e4b2aa
Miguel Malagoli <miguel.g.malagoli@gmail.com>
2020-08-29 11:44:54 -0300

    Componente do rodapé

---
+++ src/componentes/Rodape.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Rodape extends React.Component {
+    render() {
+        return (
+            <footer id="rodape">
+                <div className="txtRodape">
+                    <p>@Cocreare 2020. Todos os direitos reservados</p>
+                </div>
+            </footer>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './css/menu-rodape.css';
 import './css/sobre.css';
 
 //Componentes
-import Menu from './componentes/Sobre-menu';
+import Menu from './componentes/Menu';
+import Rodape from './componentes/Rodape';
 import SobreEquipe from './componentes/Sobre-equipe';
 import {
     BrowserRouter as Router,
@@ -22,6 +23,7 @@ ReactDOM.render(
             <Route path='/Sobre'>
               <Menu home={true} sobre={false} login={true} />
               <SobreEquipe />
+			  <Rodape />
             </Route>
             <Route path='/Login'>
               <Menu home={true} sobre={true} login={false} />
