5b421fe2918a9b5837751378d1b521e4659e5338
Geice Gomes <sogmesmo@gmail.com>
2020-09-03 22:18:57 -0300

    adequaçao imagens

---
+++ src/componentes/imagens.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+//import Imagens
+
+import Imagens from '../imagens/top10';
+
+export default class Imagens extends React.Component {
+
+    render() {
+        return (
+            <div className="containerImagem">                
+            <a target="_blank">
+                <img 
+                 src={this.props.imagem}
+                 alt="foto Postada"
+                 onclick="cliqueModal(this)"/>
+                <div className="degrade"></div>
+                <div className="containerTexto">
+                    <div className="texto">
+                        <div className="textoPostadoPor">Postado por: {this.props.textoPostadoPor}</div>
+                        <div className="textoTwitterUser">@twitterusername {this.props.textoTwitterUse}</div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        )
+    }
+}
