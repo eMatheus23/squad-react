db96b5b0281dd2198f4eb17d0e7fa93af612ab8b
julio <juli0o79@hotmail.com>
2020-09-18 09:01:25 -0300

    Adicionado comentários e indentação do código

--- src/pages/buscas/pagination.jsx
+++ src/pages/buscas/pagination.jsx
@@ -1,33 +1,19 @@
 import React from "react"
 
+//Esse código cria os links das abas de paginação da tabela
 const Pagination = ({ postsPerPage, totalPosts, paginate, currentPage}) => {
+
     const pageNumbers = [];
     for(let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++){
         pageNumbers.push(i);
     }
-
-    
-    //função pra link ativo
-
-    function active(){
-        const totalLink = document.getElementsByClassName('link');
-     const pai = document.getElementById('paginacao')// o evento só tá funcionand uma vez, tentar event delegation usando listener no elemento pi
-
-        for( let m =1; m < totalLink.length; m ++){
-            totalLink[m].addEventListener('click', function(){
-                totalLink[m].classList.remove("active")
-                this.classList.add("active");
-            })
-        }
-
-    }
-
     return(
-        
+        /*os links são gerados dinâmicamente de acordo com o número 
+        de respostas da api dívido pelo número estabelecido de posts por páginas*/
         <nav>
             <ul id ="paginacao">
                 {pageNumbers.map(number =>(
-                    <li className={currentPage === number ? 'active ' : ''} key = {number} onClick ={() => active()}>
+                    <li className={currentPage === number ? 'active ' : ''} key = {number}>
                        <a className = "" onClick={
                            ()=> paginate(number)
                        } href="javascript:;" >
