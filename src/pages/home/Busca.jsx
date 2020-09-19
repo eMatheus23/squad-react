1590dd8c6f04a311735195d41ed049ffeb1c05f0
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-19 20:00:50 -0300

    Retirado outline do input e envio da palavra

--- src/pages/home/Busca.jsx
+++ src/pages/home/Busca.jsx
@@ -1,22 +1,44 @@
-import React from 'react'
+import React from "react";
 
-function limparHashtag() {
+function limparHashtag() {}
+
+const handleKeyDown = (event) => {
+  if (event.key === "Enter" && event.target.value !== '') {
+    event.preventDefault();
     
-}
+    var termoBusca = event.target.value
+    
+    console.log(termoBusca);
+  }
+};
 
 function Busca() {
-    return <div className="hero-image">
-                <div className="hero-image-drop">
-                </div>
-                <div className="hero-text">
-                    <h1>Encontre hashtags<br/> de maneira fácil.</h1>
-                    <p>Digite o que deseja no campo de buscas e <br/>confira os resultados do Twitter abaixo</p>
-                </div>
-                <div className="hero-busca">
-                    <input type="text" id="txt-busca" onFocus={limparHashtag} className="input" placeholder="Buscar..."/>               
-                    <div className="message"></div>
-                </div>
-            </div>
+  return (
+    <div className="hero-image">
+      <div className="hero-image-drop"></div>
+      <div className="hero-text">
+        <h1>
+          Encontre hashtags
+          <br /> de maneira fácil.
+        </h1>
+        <p>
+          Digite o que deseja no campo de buscas e <br />
+          confira os resultados do Twitter abaixo
+        </p>
+      </div>
+      <div className="hero-busca">
+        <input
+          type="text"
+          id="txt-busca"
+          onFocus={limparHashtag}
+          className="input"
+          placeholder="Buscar..."
+          onKeyDown={handleKeyDown}
+        />
+        <div className="message"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Busca;
