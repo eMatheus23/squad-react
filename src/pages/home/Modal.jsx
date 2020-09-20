0c204a2ed5555c7bfec6c050edce0b10c42ea3dd
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-20 00:35:22 -0300

    Removido console.log

--- src/pages/home/Modal.jsx
+++ src/pages/home/Modal.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 export default class ModalImg extends React.Component {
     render() {
-        console.log(this.props.showmodal)
-        console.log(this.props.selectedImg)
         return (
         <div id="janela-modal" className={(this.props.showmodal ? 'janela-modal' : 'esconde-janela-modal')}>
             <span id="btn-fechar" className="fechar-modal" onClick={this.props.closeModalFunc}>X</span>
