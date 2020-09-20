a2d4137e7d8952e212b87d08dceada6be2481e6d
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-20 01:14:58 -0300

    Implementado o map() para gerar tweets

--- src/pages/home/Galeria-tweets.jsx
+++ src/pages/home/Galeria-tweets.jsx
@@ -2,112 +2,23 @@ import React from "react";
 
 import TweetsPostados from "./Tweets-postados";
 
-
 export default class GaleriaTweets extends React.Component {
   render() {
     return (
-      <div div id="tweets" className="containerGaleriaTweets">
-        <div className="containerTweets" id="Tweets1">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 0 ? this.props.posts[0].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 0 ? this.props.posts[0].user.name : ""}
-            twitterUserName={this.props.posts.length > 0 ? ("@" + this.props.posts[0].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 0 ? this.props.posts[0].text : ""}
-            link={this.props.posts.length > 0 ? `https://twitter.com/${this.props.posts[0].user.screen_name}/status/${this.props.posts[0].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
+      <>
+        <div div id="tweets" className="containerGaleriaTweets">
+          {this.props.posts.slice(0, 10).map((tweet, index) => (
+            <TweetsPostados
+              fotoPerfil={tweet.user.profile_image_url_https}
+              userName={tweet.user.name}
+              twitterUserName={"@" + tweet.user.screen_name}
+              textoTweet={tweet.text}
+              link={`https://twitter.com/${this.props.posts[0].user.screen_name}/status/${this.props.posts[0].id_str}`}
+              verMais="Ver mais no twitter"
+            />
+          ))}
         </div>
-        <div className="containerTweets" id="Tweets2">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 1 ? this.props.posts[1].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 1 ? this.props.posts[1].user.name : ""}
-            twitterUserName={this.props.posts.length > 1 ? ("@" + this.props.posts[1].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 1 ? this.props.posts[1].text : ""}
-            link={this.props.posts.length > 1 ? `https://twitter.com/${this.props.posts[1].user.screen_name}/status/${this.props.posts[1].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets3">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 2 ? this.props.posts[2].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 2 ? this.props.posts[2].user.name : ""}
-            twitterUserName={this.props.posts.length > 2 ? ("@" + this.props.posts[2].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 2 ? this.props.posts[2].text : ""}
-            link={this.props.posts.length > 2 ? `https://twitter.com/${this.props.posts[2].user.screen_name}/status/${this.props.posts[2].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets4">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 3 ? this.props.posts[3].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 3 ? this.props.posts[3].user.name : ""}
-            twitterUserName={this.props.posts.length > 3 ? ("@" + this.props.posts[3].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 3 ? this.props.posts[3].text : ""}
-            link={this.props.posts.length > 3 ? `https://twitter.com/${this.props.posts[3].user.screen_name}/status/${this.props.posts[3].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets5">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 4 ? this.props.posts[4].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 4 ? this.props.posts[4].user.name : ""}
-            twitterUserName={this.props.posts.length > 4 ? ("@" + this.props.posts[4].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 4 ? this.props.posts[4].text : ""}
-            link={this.props.posts.length > 4 ? `https://twitter.com/${this.props.posts[4].user.screen_name}/status/${this.props.posts[4].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets6">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 5 ? this.props.posts[5].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 5 ? this.props.posts[5].user.name : ""}
-            twitterUserName={this.props.posts.length > 5 ? ("@" + this.props.posts[5].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 5 ? this.props.posts[5].text : ""}
-            link={this.props.posts.length > 5 ? `https://twitter.com/${this.props.posts[5].user.screen_name}/status/${this.props.posts[5].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets7">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 6 ? this.props.posts[6].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 6 ? this.props.posts[6].user.name : ""}
-            twitterUserName={this.props.posts.length > 6 ? ("@" + this.props.posts[6].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 6 ? this.props.posts[6].text : ""}
-            link={this.props.posts.length > 6 ? `https://twitter.com/${this.props.posts[6].user.screen_name}/status/${this.props.posts[6].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets8">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 7 ? this.props.posts[7].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 7 ? this.props.posts[7].user.name : ""}
-            twitterUserName={this.props.posts.length > 7 ? ("@" + this.props.posts[7].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 7 ? this.props.posts[7].text : ""}
-            link={this.props.posts.length > 7 ? `https://twitter.com/${this.props.posts[7].user.screen_name}/status/${this.props.posts[7].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets9">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 8 ? this.props.posts[8].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 8 ? this.props.posts[8].user.name : ""}
-            twitterUserName={this.props.posts.length > 8 ? ("@" + this.props.posts[8].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 8 ? this.props.posts[8].text : ""}
-            link={this.props.posts.length > 8 ? `https://twitter.com/${this.props.posts[8].user.screen_name}/status/${this.props.posts[8].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-        <div className="containerTweets" id="Tweets10">
-          <TweetsPostados
-            fotoPerfil={this.props.posts.length > 9 ? this.props.posts[9].user.profile_image_url_https : ""}
-            userName={this.props.posts.length > 9 ? this.props.posts[9].user.name : ""}
-            twitterUserName={this.props.posts.length > 9 ? ("@" + this.props.posts[9].user.screen_name) : ""}
-            textoTweet={this.props.posts.length > 9 ? this.props.posts[9].text : ""}
-            link={this.props.posts.length > 9 ? `https://twitter.com/${this.props.posts[9].user.screen_name}/status/${this.props.posts[9].id_str}` : ""}
-            verMais="Ver Mais Tweets"
-          />
-        </div>
-      </div>
+      </>
     );
   }
 }
--- src/pages/home/Tweets-postados.jsx
+++ src/pages/home/Tweets-postados.jsx
@@ -3,17 +3,26 @@ import React from "react";
 export default class TweetsPostados extends React.Component {
   render() {
     return (
-      <div className="grupoTweets">
-        <img src={this.props.fotoPerfil} alt="imagem" />
-        <div className="containerTextosTwitter">
-          <div className="containerUser">
-            <div className="userName">{this.props.userName}</div>
-            <div className="twitterUserName">{this.props.twitterUserName}</div>
+      <div className="containerTweets" id="Tweets1">
+        <div className="grupoTweets">
+          <img src={this.props.fotoPerfil} alt="imagem" />
+          <div className="containerTextosTwitter">
+            <div className="containerUser">
+              <div className="userName">{this.props.userName}</div>
+              <div className="twitterUserName">
+                {this.props.twitterUserName}
+              </div>
+            </div>
+            <div className="textoTweet">{this.props.textoTweet}</div>
+            <a
+              href={this.props.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkTwitter"
+            >
+              {this.props.verMais}
+            </a>
           </div>
-          <div className="textoTweet">{this.props.textoTweet}</div>
-          <a href={this.props.link} target="_blank" rel="noopener noreferrer" className="linkTwitter">
-            {this.props.verMais}
-          </a>
         </div>
       </div>
     );
