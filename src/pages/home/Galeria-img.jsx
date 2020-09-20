7e05d6db4e95ef80fef7c62f75ddeea8c7401899
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-20 01:14:02 -0300

    Retiradas imagens não utilizadas e console

--- src/pages/home/Galeria-img.jsx
+++ src/pages/home/Galeria-img.jsx
@@ -3,17 +3,6 @@ import React from "react";
 //componets
 import ImagensPostadas from "./Imagens-postadas";
 import ModalImg from "./Modal";
-//imagens
-import PrmeiraImagen from "../../imagens/top10/photo-01.jpg";
-import SegundaImagen from "../../imagens/top10/photo-02.jpg";
-import TerceiraImagen from "../../imagens/top10/photo-03.jpg";
-import QuartaImagen from "../../imagens/top10/photo-04.jpg";
-import QuintaImagen from "../../imagens/top10/photo-05.jpg";
-import SextaImagen from "../../imagens/top10/photo-06.jpg";
-import SetimaImagen from "../../imagens/top10/photo-07.jpg";
-import OitavaImagen from "../../imagens/top10/photo-08.jpg";
-import NonaImagen from "../../imagens/top10/photo-09.jpg";
-import DecimaImagen from "../../imagens/top10/photo-10.jpg";
 
 export default class GaleriaImagens extends React.Component {
   constructor(props) {
@@ -40,7 +29,6 @@ export default class GaleriaImagens extends React.Component {
   }
 
   render() {
-    console.log(this.props.posts);
     return (
       //Container que segura todas as divs de imagens relacionadas na pesquisadas
       <>
