1eef1bb1149db40839700b9d0469342f96f9a1e0
Elias Oliveira <eliasmatheus@hotmail.com>
2020-09-20 01:15:16 -0300

    Adicionado href em link

--- src/pages/home/Modal.jsx
+++ src/pages/home/Modal.jsx
@@ -6,7 +6,7 @@ export default class ModalImg extends React.Component {
         <div id="janela-modal" className={(this.props.showmodal ? 'janela-modal' : 'esconde-janela-modal')}>
             <span id="btn-fechar" className="fechar-modal" onClick={this.props.closeModalFunc}>X</span>
             <img id="img-modal" className="img-modal" src={this.props.selectedImg} alt="" />
-            <a>
+            <a href="">
                 <span id="visitar-tweet" className="visitar-tweet">
                     Ver no Twitter
                 </span>
